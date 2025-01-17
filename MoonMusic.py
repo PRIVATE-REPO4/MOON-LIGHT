407f5a82428dfb91a0bc2af7e6dc2e35b35e735d
𝐒𝐘𝐍𝐓𝐀𝐗 ＜∕＞ <166818922+syntaxworldcodes@users.noreply.github.com>
2025-01-17 20:03:03 +0530

    Update MoonMusic.py

--- MoonMusic.py
+++ MoonMusic.py
@@ -804,6 +804,47 @@ async def stream_logger(
 
 # Change stream & Close Stream
 
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "◉—————————"
+    elif 10 < umm < 20:
+        bar = "—◉————————"
+    elif 20 <= umm < 30:
+        bar = "——◉———————"
+    elif 30 <= umm < 40:
+        bar = "———◉——————"
+    elif 40 <= umm < 50:
+        bar = "————◉—————"
+    elif 50 <= umm < 60:
+        bar = "—————◉————"
+    elif 60 <= umm < 70:
+        bar = "——————◉———"
+    elif 70 <= umm < 80:
+        bar = "———————◉——"
+    elif 80 <= umm < 95:
+        bar = "————————◉—"
+    else:
+        bar = "—————————◉"
+    buttons = [
+       [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ], 
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+    ]
+    return buttons
 
 async def change_stream(chat_id):
     queued = QUEUE.get(chat_id)
@@ -860,7 +901,7 @@ async def change_stream(chat_id):
             [
                 InlineKeyboardButton(
                     text="🗑️ Close",
-                    callback_data="force_close",
+                    callback_data="stream_markup_timer",
                 )
             ],
         ]
